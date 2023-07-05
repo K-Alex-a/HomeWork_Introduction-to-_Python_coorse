a2540d6fba4850da7ac81db622f3544bb9177fbe
K_aLex-a <shiotoy@icloud.com>
2023-07-05 12:45:00 +0300

    complete task_32

--- Seminar_06/task_32.py
+++ Seminar_06/task_32.py
@@ -10,3 +10,20 @@
 0, -5, -5, 7]
 Вывод: [1, 9, 13, 14, 19]
 ============================================="""
+from random import randint
+
+lst = [randint(-10, 10) for i in range(randint(10, 20))]
+print(lst)
+
+range_search = list(map(int, input('Input range from X to Y: ').split()))
+
+max_range = max(range_search)
+min_range = min(range_search)
+print(f'start = {min_range}, stop = {max_range}')
+
+
+def index_in_range(lists, start, stop):
+    return [(index, lst[index]) for index in range(len(lst)) if start <= lists[index] <= stop]
+
+
+print(index_in_range(lst, min_range, max_range))
